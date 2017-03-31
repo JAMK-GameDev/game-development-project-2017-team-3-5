4f832414d177fd09ce56fc6308be32d702b11e7a
Withee Poositasai <L1348@LABRANET.JAMK.FI>
2017-03-31 15:16:05 +0300

    Continue UI, add button, toggle animation

---
+++ Assets/Resources/UI/script/paperController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paperController : MonoBehaviour {
+
+    Animator anim;
+
+    void Start()
+    {
+        //Initialize boolean
+        anim = GetComponent<Animator>();
+        anim.SetBool("isEnable", false);
+    }
+
+    public void toggle()
+    {
+        //Reverse Boolean
+        anim.SetBool("isEnable", !anim.GetBool("isEnable"));
+    }
+}
