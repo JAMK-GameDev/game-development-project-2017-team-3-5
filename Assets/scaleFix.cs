11a46cccc1df5e341921cd839011f6b2d58172fa
Juha-Matti Kokkonen <juhamk91@gmail.com>
2017-04-24 10:44:49 +0300

    Models have colour and game is in a bigger size, scales back to small to fix some bugs.

---
+++ Assets/scaleFix.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scaleFix : MonoBehaviour {
+
+	// Scale the game for more managable mode, this scales it back to small so the bugs will fix themselves.
+	void Start () {
+		transform.localScale = new Vector3 (1, 1, 1);
+	}
+}
